4dd72d519376859711d565aa33817a1405c51ec1
Rishabh Jha <rishabhjha1811@gmail.com>
2026-01-28 15:48:22 +0530

    HyperParameter Tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    print(modeltrainer.hyper_parameter(train_arr,test_arr))
 
     
 
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,6 +11,8 @@ from catboost import CatBoostRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 from src.utils import save_object, evaluate_model
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
 
 @dataclass
 class ModelTrainerConfig: ## We are making the config file every time so that code will be flexiblle , clean and easy to read
@@ -35,13 +37,16 @@ class ModelTrainer:
             models={
     'Linearregressor': LinearRegression(),
     'Lasso': Lasso(),
-    'Ridge': Ridge(),
+    'Ridge': Ridge(alpha=2.0,solver='lsqr'),
     'KNeighborsRegressor': KNeighborsRegressor(),
     'DecisionTreeRegressor': DecisionTreeRegressor(),
     'RandomForestRegressor': RandomForestRegressor(),
     # 'CatBoostingRegressor',CatBoostRegressor(verbose=False),
     'AdaBoostingRegressor': AdaBoostRegressor()
 }
+            
+            
+           
             
             model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
             logging.info("After the model")
@@ -60,23 +65,68 @@ class ModelTrainer:
             
             logging.info("Best found model on both training and testing dataset")
 
+            
+        
+    
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-
-
             predicted=best_model.predict(x_test)
             r2=r2_score(y_test,predicted)
             return r2, best_model
+            
+        except Exception as e:
+            raise CustomException(e,sys)
         
 
 
+    def hyper_parameter(self,train_array,test_array):
+        try:
+            logging.info("Spliting training and test input data")
+            x_train,y_train,x_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models={
+    'Linearregressor': LinearRegression(),
+    'Lasso': Lasso(),
+    'Ridge': Ridge(alpha=1.0, solver='saga'),
+    'KNeighborsRegressor': KNeighborsRegressor(),
+    'DecisionTreeRegressor': DecisionTreeRegressor(),
+    'RandomForestRegressor': RandomForestRegressor(),
+    # 'CatBoostingRegressor',CatBoostRegressor(verbose=False),
+    'AdaBoostingRegressor': AdaBoostRegressor()
+}
+            
+            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            ## To get the best model score we are using this 
+            best_model_score= max(model_report.values())
+            ## To get the best modle name we are using this
 
+            best_model_name= list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
 
+            ]
 
+            best_model = models[best_model_name]
+            logging.info("Params is ready")
+            params={
+                'alpha':[1.0,2.0,3.0,4.0,5.0],
+                'solver':['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga', 'lbfgs']
+            }
+            cv=StratifiedKFold(5)
+            logging.info("StraightkFold is used directly")
+            grid=GridSearchCV(best_model,param_grid=params,cv=cv,n_jobs=-1)
+            grid_param=grid.fit(x_train,y_train)
+            logging.info("The HyperParameter is over")
+            return grid_param.best_score_, grid_param.best_params_
+        
         except Exception as e:
             raise CustomException(e,sys)
+        
 
 
 
